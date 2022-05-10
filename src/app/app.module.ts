0c5e33073445aaf2aa622ab0cf8aa6db97649958
Túlio <bastostuliojrrt@gmail.com>
2022-05-09 23:10:05 -0300

    subindo o código

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './features/cadastro/cadastro.component';
+import { LoginComponent } from './features/login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import { CadastroFornecedorComponent } from './features/cadastro-fornecedor/cadastro-fornecedor.component';
+import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component';
+import {InputNumberModule} from 'primeng/inputnumber';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    LoginComponent,
+    CadastroFornecedorComponent,
+    CadastroClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule,
+    PasswordModule,
+    InputNumberModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
